5f54da80fec1f34a6bbc43a3316d080aa086116b
ry3879 <rachel.y.3879@gmail.com>
2019-09-14 01:46:35 -0400

    top 5 eatery buttons

--- app/src/main/java/com/example/hackcmu19/DisplayMessageActivity.java
+++ app/src/main/java/com/example/hackcmu19/DisplayMessageActivity.java
@@ -5,8 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.view.View;
+import android.widget.Button;
 
-public class DisplayMessageActivity extends AppCompatActivity {
+public class DisplayMessageActivity extends AppCompatActivity{
+    public static final String EXTRA_MESSAGE3 = "com.example.hackcmu19.MESSAGE3";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +27,37 @@ public class DisplayMessageActivity extends AppCompatActivity {
         TextView topLabel = findViewById(R.id.secondScreenMessage);
         topLabel.setText("You entered: walking for " + msgDistMinutes +
                 "min and waiting for " + msgWaitingMinutes + "min");
+
+    }
+    public void goToPage(View view)
+    {
+        Intent intentButton = new Intent(this, DisplayButtonActivity.class);
+        Button b = (Button) findViewById(R.id.eatery1);
+
+        String eateryName;
+
+        switch (view.getId()) {
+            case R.id.eatery1:
+                b = (Button) findViewById(R.id.eatery1);
+                break;
+            case R.id.eatery2:
+                b = (Button) findViewById(R.id.eatery2);
+                break;
+            case R.id.eatery3:
+                b = (Button) findViewById(R.id.eatery3);
+                break;
+            case R.id.eatery4:
+                b = (Button) findViewById(R.id.eatery4);
+                break;
+            case R.id.eatery5:
+                b = (Button) findViewById(R.id.eatery5);
+                break;
+
+        }
+        //b = (Button) findViewById(view.getId());
+        eateryName = b.getText().toString();
+
+        intentButton.putExtra(EXTRA_MESSAGE3, eateryName);
+        startActivity(intentButton);
     }
 }
